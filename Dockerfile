be4d5462198184d47b6317da1e4a80a0946054cd
Baiju-R <rs989627@gmail.com>
2026-04-30 09:49:53 +0000

    first commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM php:8.2-apache
+
+# Install mysqli extension
+RUN docker-php-ext-install mysqli
+
+# Copy project files
+COPY . /var/www/html/
+
+# Enable Apache rewrite
+RUN a2enmod rewrite
+
+# Set permissions
+RUN chown -R www-data:www-data /var/www/html
---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+
+$servername = "mysql";   // ✅ Kubernetes service name
+$username = "root";
+$password = "root";
+$db = "onlineshop";
+
+$con = mysqli_connect($servername, $username, $password, $db);
+
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
